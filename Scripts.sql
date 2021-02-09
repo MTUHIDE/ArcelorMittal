063ad4e3b953d2735b16a01a58d7b08dd3223fe1
bahansen-mtu <55208702+bahansen-mtu@users.noreply.github.com>
2021-02-09 12:09:43 -0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -1,3 +1,8 @@
+/* Populate the CompanyName field using CustomerName */
 UPDATE TSESummary SET CompanyName = IF( LENGTH(CustomerName) - LENGTH(REPLACE(CustomerName,'-','')) > 1, SUBSTRING_INDEX( CustomerName, '-', 2 ), SUBSTRING_INDEX( CustomerName, '-', 1 ) );
 
+/* Populate the Location field using CustomerName */
 UPDATE TSESummary SET Location = IF( ( LENGTH(CustomerName) - LENGTH(REPLACE(CustomerName,'-','')) = 0 ), '', IF( LENGTH(CustomerName) - LENGTH(REPLACE(CustomerName,'-','')) > 1, SUBSTRING_INDEX( CustomerName, '-', -2 ), SUBSTRING_INDEX( CustomerName, '-', -1 ) ) );
+
+/* Count the number of CustomerNames with more than 2 '-' characters */
+SELECT COUNT( CustomerName ) AS MoreThan3Dashes FROM  TSESummary WHERE ( LENGTH(CustomerName) - LENGTH(REPLACE(CustomerName,'-','')) ) > 2;

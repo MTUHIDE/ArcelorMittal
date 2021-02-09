f98d5f5dda4e4c028e2c9175c78c8f13ea501643
bahansen-mtu <55208702+bahansen-mtu@users.noreply.github.com>
2021-02-09 11:44:53 -0500

    Add files via upload

---
+++ Scripts.sql
@@ -0,0 +1,3 @@
+UPDATE TSESummary SET CompanyName = IF( LENGTH(CustomerName) - LENGTH(REPLACE(CustomerName,'-','')) > 1, SUBSTRING_INDEX( CustomerName, '-', 2 ), SUBSTRING_INDEX( CustomerName, '-', 1 ) );
+
+UPDATE TSESummary SET Location = IF( LENGTH(CustomerName) - LENGTH(REPLACE(CustomerName,'-','')) > 1, SUBSTRING_INDEX( CustomerName, '-', -1 ), SUBSTRING_INDEX( CustomerName, '-', -1 ) );

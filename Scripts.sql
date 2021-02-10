78abf385b9013eec22d6c500b9de0c8a53cca156
bahansen-mtu <55208702+bahansen-mtu@users.noreply.github.com>
2021-02-10 17:58:07 -0500

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -6,3 +6,18 @@ UPDATE TSESummary SET Location = IF( ( LENGTH(CustomerName) - LENGTH(REPLACE(Cus
 
 /* Count the number of CustomerNames with more than 2 '-' characters */
 SELECT COUNT( CustomerName ) AS MoreThan3Dashes FROM  TSESummary WHERE ( LENGTH(CustomerName) - LENGTH(REPLACE(CustomerName,'-','')) ) > 2;
+
+/* Select specific fields */
+SELECT a.TSEName, a.City, a.State, a.CustomerName, a.CompanyName, a.Location, b.latitude, b.longitude
+FROM TSESummary AS a
+LEFT JOIN geoname AS b
+ON LOWER(a.State) = LOWER(b.admin1code) AND LOWER(a.Location) = LOWER(b.asciiname)
+WHERE b.geonameid IS NOT NULL
+
+/* Select all fields */
+SELECT *
+FROM TSESummary AS a
+LEFT JOIN geoname AS b
+ON LOWER(a.State) = LOWER(b.admin1code)
+AND LOWER(a.Location) = LOWER(b.asciiname)
+WHERE b.geonameid IS NOT NULL

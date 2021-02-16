80ca23b5130b7d19aa3b34261dcd18a56b6c509e
mace1099 <mtsayles@mtu.edu>
2021-02-16 08:14:56 -0500

    Added js file with functions to pull info from database and the corresponding php files, also added layers and filters in mapquest.js in its current form. Changes will be made to improve it soon

--- mapquest.js
+++ mapquest.js
@@ -18,10 +18,13 @@ var data = [
 // MapQuest API key
 const key = "imdY24Bg00uiYRbaHSd066pf3JcyySBn";
 
-let nameUnique = [];
-nameUniqueOrdered = [
+var nameUnique = [];
+var nameUniqueOrdered = [
     [nameUnique[0], timesIn(nameUnique[0])]
 ];
+var map;
+var featureGroup;
+var layer;
 
 // Gets coordinates of given city/state
 // Returns MapQuest response, employee name, and customer name
@@ -56,17 +59,17 @@ function strToColor(str) {
     return color;
 }
 
-// Performs all inner actions once the HTML page is loaded
-document.addEventListener("DOMContentLoaded", function() {
+//Performs the inital load of the map when loading the site
+function initialMapLoad(data){
     L.mapquest.key = key;
 
     // 'map' refers to a <div> element with the ID map
-    const map = L.mapquest.map('map', {
+    map = L.mapquest.map('map', {
         center: [39, -96],
         layers: L.mapquest.tileLayer('map'),
         zoom: 4
     });
-
+    layer = L.layerGroup().addTo(map);
     for (let i = 0; i < data.length; i++) {
         // Stores each item in current customer array in its own variable
         let name = data[i][0];
@@ -83,6 +86,66 @@ document.addEventListener("DOMContentLoaded", function() {
         // Fetches location data from MapQuest
         getCoor(city, state, name, customer).then(fromData => {
             let latLng = fromData[0].results[0].locations[0].displayLatLng;
+            let marker = L.marker([latLng.lat, latLng.lng], {
+                text: fromData[1],
+                subtext: fromData[2],
+                position: 'down',
+                type: 'marker',
+                icon: L.mapquest.icons.marker({
+                    primaryColor: strToColor(fromData[1]),
+                    secondaryColor: strToColor(fromData[0].results[0].providedLocation.location),
+                    size: 'sm'
+                })
+            }).addTo(layer);
+             // Assign a popup with customer's information to appear above customer's map marker on click
+             let popupContent = '<div>' + city + ', ' + state + '</div><div>' + latLng.lat + ', ' + latLng.lng + '</div><div>' + customer + '</div><div>' + name + '</div>';
+             marker.bindPopup(popupContent).openPopup();
+        });
+    }
+
+    for (let i = 1; i < nameUnique.length; i++) {
+        let count = timesIn(nameUnique[i]);
+
+        for (let j = 0; j < nameUniqueOrdered.length; j++) {
+            if (count >= nameUniqueOrdered[j][1]) {
+                nameUniqueOrdered.splice(j, 0, [nameUnique[i], count]);
+                break;
+            } else if (j == nameUniqueOrdered.length - 1) {
+                nameUniqueOrdered.push([nameUnique[i], count]);
+                break;
+            }
+        }
+
+    }
+
+    for (let i = 0; i < nameUniqueOrdered.length; i++) {
+        document.getElementById("people").innerHTML += '<div class="subpeople" style="margin: 5px; padding-left: 2px; font-size: 15px; border-style: solid; border-color: ' + strToColor(nameUniqueOrdered[i][0]) + ';">' + nameUniqueOrdered[i][0] + ' (' + nameUniqueOrdered[i][1] + ') </div>';
+    }
+}
+
+//Loads results of filter into the map
+function loadIntoMap(people){
+    //Reset values 
+    console.log(people);
+    nameUnique = [];
+    nameUniqueOrdered = [
+     [nameUnique[0], timesIn(nameUnique[0])]
+    ];
+    layer.clearLayers();
+    layer = L.layerGroup().addTo(map);
+    for (let i = 0; i < people.length; i++) {
+        let name = people[i][0];
+        let customer = people[i][1];
+        let city = people[i][2];
+        let state = people[i][3];
+        let country = people[i][4];
+
+        if (!nameUnique.includes(name)) {
+            nameUnique.push(name);
+        }
+
+        getCoor(city, state, name, customer).then(fromData => {
+            let latLng = fromData[0].results[0].locations[0].displayLatLng;
 
             // Adds customer marker to map with returned info
             let marker = L.mapquest.textMarker([latLng.lat, latLng.lng], {
@@ -90,16 +153,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 subtext: fromData[2],
                 position: 'down',
                 type: 'marker',
-                icon: {
+                icon: L.mapquest.icons.marker({
                     primaryColor: strToColor(fromData[1]),
                     secondaryColor: strToColor(fromData[0].results[0].providedLocation.location),
                     size: 'sm'
-                }
-            }).addTo(map);
+                })
+            }).addTo(layer);
 
-            // Assign a popup with customer's information to appear above customer's map marker on click
-            let popupContent = '<div>' + city + ', ' + state + '</div><div>' + latLng.lat + ', ' + latLng.lng + '</div><div>' + customer + '</div><div>' + name + '</div>';
-            marker.bindPopup(popupContent).openPopup();
+             // Assign a popup with customer's information to appear above customer's map marker on click
+             let popupContent = '<div>' + city + ', ' + state + '</div><div>' + latLng.lat + ', ' + latLng.lng + '</div><div>' + customer + '</div><div>' + name + '</div>';
+             marker.bindPopup(popupContent).openPopup();
         });
     }
 
@@ -123,4 +186,6 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let i = 0; i < nameUniqueOrdered.length; i++) {
         document.getElementById("people").innerHTML += '<div class="subpeople" style="margin: 5px; padding-left: 2px; font-size: 15px; border-style: solid; border-color: ' + strToColor(nameUniqueOrdered[i][0]) + ';">' + nameUniqueOrdered[i][0] + ' (' + nameUniqueOrdered[i][1] + ') </div>';
     }
-});
+}
+
+document.addEventListener("DOMContentLoaded", function() {  initialMapLoad(data)});
